5076217343e1c1e86bc577c9cd1d680b615b3410
calmkeen <iyunseong159@gmail.com>
2022-04-05 13:29:36 +0900

    Initial Commit

---
+++ NoteAppSub/NoteAppSubApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppSubApp.swift
+//  NoteAppSub
+//
+//  Created by calmkeen on 2022/04/05.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppSubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
